fc18884f229fdef1c123a298efea759d5b7cca29
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2023-02-14 10:06:49 -0500

    firebase service acount key

---
+++ components/ClientProvider.tsx
@@ -0,0 +1,13 @@
+// "use client";
+
+
+// // import { Toaster } from "react-hot-toast";
+// import toast, { Toaster } from "react-hot-toast";
+
+// export default function ClientProvider() {
+//   return (
+//     <>
+//       <Toaster position="top-right" />
+//     </>
+//   );
+// }
